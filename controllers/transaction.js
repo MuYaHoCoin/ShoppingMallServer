0b07208517fab3b7bd655860739d4eb93fcafb4d
hoho00 <961boss@gmail.com>
2021-09-10 11:06:45 +0900

    some

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,4 +1,6 @@
 const transactionModel = require("../model/transaction");
+const make_address = require("../functions/key_management/make_address");
+const qr = require("qr-image");
 
 module.exports = {
   getAllTransactions: (req, res) => {
@@ -60,4 +62,41 @@ module.exports = {
       });
     }
   },
+
+  getNewIncomingAddress : (req, res) => {
+    try {
+      const addr = make_address();
+      res.status(200);
+      res.send({
+        success: true,
+        newaddr: addr,
+      });
+    } catch (error) {
+      console.log(`Error : ${error}`);
+      res.status(400);
+      res.send({
+        success: false,
+        message: `Error :  ${error}`,
+      });
+    }
+  },
+
+  getQRCode : (req, res) => {
+    try{
+      const qr_image = qr.imageSync("hihi", {type: 'svg'});
+      res.status(200);
+      res.send({
+        success: true,
+        newaddr: qr_image,
+      });
+    } catch(error) {
+      console.log(`Error : ${error}`);
+      res.status(400);
+      res.send({
+        success: false,
+        message: `Error :  ${error}`,
+      });
+    }
+  }
+
 };
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const port = 3001;
 
 const router = require("./routes/routes");
 app.use(express.json());
+app.use(cors());
 app.use("/api", router);
 
 app.listen(port, () => console.log(`port : ${port}`));
